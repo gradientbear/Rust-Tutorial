2ee81a6197f1e8c31a2460482ef7953175245ad8
gradientbear <jack.h.tamm@gmail.com>
2025-08-23 15:28:25 -0400

    complete ownership_sample

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ownership_sample;
 
 fn main(){
-    ownership_sample::fn1();
+    // ownership_sample::fn1();
+    ownership_sample::fn2();
 }
--- src/ownership_sample.rs
+++ src/ownership_sample.rs
@@ -1,10 +1,23 @@
 pub fn fn1 () {
     let v = vec![1,2,3];     // vector v owns the object in heap
     let v2 = v;              // moves ownership to v2
-    display(v2);             // v2 is moved to display and v2 is invalidated
-    println!("In main {:?}",v2);    //v2 is No longer usable here
+    display1(v2);             // v2 is moved to display and v2 is invalidated
+    // println!("In main {:?}",v2);    //v2 is No longer usable here
 }
 
-fn display(v:Vec<i32>){
+fn display1(v:Vec<i32>){
    println!("inside display {:?}",v);
+}
+
+pub fn fn2(){
+   let v = vec![1,2,3];       // vector v owns the object in heap
+   let v2 = v;                // moves ownership to v2
+   let v2_return = display2(v2);    
+   println!("In main {:?}",v2_return);
+}
+
+fn display2(v:Vec<i32>)->Vec<i32> { 
+   // returning same vector
+   println!("inside display {:?}",v);
+   v
 }
