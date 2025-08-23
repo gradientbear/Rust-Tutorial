51eb823e58f2c254d93ab59b1de8fb90d947342d
gradientbear <jack.h.tamm@gmail.com>
2025-08-23 09:37:52 -0400

    tutor template

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
+mod ownership_sample;
+
 fn main(){
-   let v = vec![1,2,3];     // vector v owns the object in heap
-   let v2 = v;              // moves ownership to v2
-   display(v2);             // v2 is moved to display and v2 is invalidated
-   println!("In main {:?}",v2);    //v2 is No longer usable here
-}
-fn display(v:Vec<i32>){
-   println!("inside display {:?}",v);
+    ownership_sample::fn1();
 }
